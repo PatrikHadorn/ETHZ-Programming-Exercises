52136c81a5f7ce63c1ed5f2c0be2ecc68f048ce0
Patrik Hadorn <hadornpa@slabhg143.ethz.ch>
2017-03-08 13:59:51 +0100

    Working on Blum-Algorithm implementation

--- dna/ex2_4/main.cpp
+++ dna/ex2_4/main.cpp
@@ -1,23 +1,81 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
-std::string f(unsigned n) 
+class Selector
 {
-	if (n == 0)
-		return "g0+g0";
-	if (n == 1)
-		return "g1";
+public:
+	Selector(std::vector<int> const& a);
 
-	std::string a = f(n - 1);
-	std::string b = f(n - 2);
-	std::string c = f(n - 2);
+	int selectRandomize(size_t k);
+	int selectBlum(size_t k);
+	int selectBruteForce(size_t k);
 
-	return "g(" + a + "+" + b + "+" + c + ")";
+private:
+	std::vector<int> m_array;
+
+};
+
+Selector::Selector(std::vector<int> const& a) : m_array(a)
+{
+}
+
+int Selector::selectRandomize(size_t k)
+{
+	return 0;
+}
+
+int Selector::selectBlum(size_t k)
+{
+	// Calculate Median of Groups of 5
+	std::vector<int> medians(m_array.size()/5+1);
+	int tmp[5];
+	for (int j=0; j<m_array.size(); j+=5)
+	{
+		std::copy(&m_array[j], &m_array[j+5], tmp);
+		std::sort(tmp, tmp+5);
+		medians[j/5] = tmp[2];
+	}
+
+	std::cout << "Medians: [";
+	for (int el : medians)
+		std::cout << el << " ";
+	std::cout << "]\n";
+
+	// Calculate Median of Medians
+	return 0;
+}
+
+int Selector::selectBruteForce(size_t k)
+{
+	return 0;
+}
+
+void print_array(std::vector<int> const& array)
+{
+	std::cout << "[";
+	for (auto el : array)
+		std::cout << el << " ";
+	std::cout << "]\n";
 }
 
 int main(int argc, char* argv[])
 {
-	std::cout << f(atoi(argv[1])) << std::endl;
+	int size;
+	std::cin >> size;
+	std::vector<int> array(size);
+	for (int i=0; i<size; ++i)
+		std::cin >> array[i];
+
+	int selection;
+	std::cin >> selection;
+
+	print_array(array);
+
+	Selector selector(array);
+	selector.selectBlum(5);
+
 	return 0;
 }
 
