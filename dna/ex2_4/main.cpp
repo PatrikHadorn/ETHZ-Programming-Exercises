e371b7c5476bf927793f575566a69262a8171cae
Patrik Hadorn <hadornpa@ethz.ch>
2017-03-10 16:30:30 +0100

    adding pivot selection algorithms and basic select algorithm

--- dna/ex2_4/main.cpp
+++ dna/ex2_4/main.cpp
@@ -1,64 +1,135 @@
+#include <algorithm>
+#include <cassert>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 
-class Selector
+template<typename T> class Selector
 {
 public:
-	Selector(std::vector<int> const& a);
+	using value_type = typename T::value_type;
+	using container_type = T;
+	using iterator_type = typename T::iterator;
 
-	int selectRandomize(size_t k);
-	int selectBlum(size_t k);
-	int selectBruteForce(size_t k);
+public:
+	Selector(T const& a) : m_array(a) {};
+
+	value_type selectRandomize(size_t k);
+	value_type selectBlum(size_t k);
+	value_type selectBruteForce(size_t k);
 
 private:
-	std::vector<int> m_array;
+	T m_array;
 
-};
-
-Selector::Selector(std::vector<int> const& a) : m_array(a)
-{
-}
-
-int Selector::selectRandomize(size_t k)
-{
-	return 0;
-}
-
-int Selector::selectBlum(size_t k)
-{
-	// Calculate Median of Groups of 5
-	std::vector<int> medians(m_array.size()/5+1);
-	int tmp[5];
-	for (int j=0; j<m_array.size(); j+=5)
+	// Pre: pivot is an element in the range [first, last)
+	// Post: Elements that compare '<' to pivot element are to the left of the pivot
+	iterator_type partition(
+			iterator_type first, iterator_type last, value_type const pivot)
 	{
-		std::copy(&m_array[j], &m_array[j+5], tmp);
-		std::sort(tmp, tmp+5);
-		medians[j/5] = tmp[2];
+		// Find Pivot position
+		auto pivotIt = std::find(first, last+1, pivot);
+		assert(pivotIt != last);
+
+		// Move Pivot to right
+		std::swap(*pivotIt, *last);
+
+		auto writeIter = first;
+		for (auto it = first; it != last; ++it)
+		{
+			if (*it < pivot)
+			{
+				std::swap(*writeIter, *it); // TODO: Unnecessary swaps
+				++writeIter;
+			}
+		}
+
+		// Move Pivot in-between
+		std::swap(*last, *writeIter);
+		return writeIter;
 	}
 
-	std::cout << "Medians: [";
-	for (int el : medians)
-		std::cout << el << " ";
-	std::cout << "]\n";
+	template<typename Func> int select(size_t k, Func pivotStrategy)
+	{
+		auto first = m_array.begin(), last = m_array.end()-1;
+
+		while (first != last)
+		{
+			auto pivot = pivotStrategy(first, last, k);
+			auto pivotIt = partition(first, last, pivot);
+
+			if (pivotIt-m_array.begin() == k)
+				return *pivotIt;
+
+			if (k < pivotIt - m_array.begin())
+				last = pivotIt-1;
+			else
+				first = pivotIt+1;
+		}
+		return *first;
+	}
+
+	static value_type simplePivot(iterator_type first, iterator_type last, int index)
+	{
+		return *first;
+	}
+	
+	static value_type randomPivot(iterator_type first, iterator_type last, int index)
+	{
+		auto pivotIt = first;
+		int count;
+		do {
+			count = 0;
+			auto const pivot = *pivotIt;
+			for (auto it = first; it != last; ++it)
+				count += (*it < pivot);
+			++pivotIt;
+		}
+		while (pivotIt < last && (count < (last-first+1)/4 || count > 3*(last-first+1)/4));
+
+		return *(pivotIt-1);
+	}
+};
+
+template<typename T> typename Selector<T>::value_type 
+Selector<T>::selectRandomize(size_t k)
+{
+	return select(k, randomPivot);
+}
+
+template<typename T> typename Selector<T>::value_type 
+Selector<T>::selectBlum(size_t k)
+{
+	// Calculate Median of Groups of 5
+	//std::vector<int> medians(m_array.size()/5+1);
+	//int tmp[5];
+	//for (size_t j=0; j<m_array.size(); j+=5)
+	//{
+	//	std::copy(&m_array[j], &m_array[j+5], tmp);
+	//	std::sort(tmp, tmp+5);
+	//	medians[j/5] = tmp[2];
+	//}
+
+	//std::cout << "Medians: [";
+	//for (int el : medians)
+	//	std::cout << el << " ";
+	//std::cout << "]\n";
 
 	// Calculate Median of Medians
 	return 0;
 }
 
-int Selector::selectBruteForce(size_t k)
+template<typename T> typename Selector<T>::value_type 
+Selector<T>::selectBruteForce(size_t k)
 {
-	return 0;
+	// Whatever's meant by brute force
+#if 1
+	return select(k, simplePivot);
+#else
+	std::sort(m_array.begin(), m_array.end());
+	return m_array[k];
+#endif
 }
 
-void print_array(std::vector<int> const& array)
-{
-	std::cout << "[";
-	for (auto el : array)
-		std::cout << el << " ";
-	std::cout << "]\n";
-}
 
 int main(int argc, char* argv[])
 {
@@ -70,11 +141,13 @@ int main(int argc, char* argv[])
 
 	int selection;
 	std::cin >> selection;
+	std::cout << "Selection: " << selection << std::endl;
 
-	print_array(array);
+	Selector<std::vector<int>> selector1(array);
+	std::cout << "Brute Force: " << selector1.selectBruteForce(selection) << std::endl;
 
-	Selector selector(array);
-	selector.selectBlum(5);
+	Selector<std::vector<int>> selector2(array);
+	std::cout << "Randomize: " << selector2.selectRandomize(selection) << std::endl;
 
 	return 0;
 }
