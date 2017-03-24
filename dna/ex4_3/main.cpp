d47e5c502d7de5d08cbce34959060f64ac7b1d39
Patrik Hadorn <hadornpa@ethz.ch>
2017-03-24 15:07:52 +0100

    Fist SkipList implementation

--- dna/ex4_3/main.cpp
+++ dna/ex4_3/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <cstring>
 #include <cassert>
+#include <cmath>
+
+// TODO: Code Deduplication by adding find(...) method used by insert and erase
+// TODO: Overhead of structure updates is very large, should add functionality 
+//       update once after structure has been filled with data
 
 template<class T,size_t MaxHeight=16> class SkipList
 {
@@ -69,6 +74,8 @@ public:
 			// first entry into list. Create new node and connect bottom layer
 			SkipListNode* newNode = createNode(val);
 			m_entryPoints[0] = newNode;
+			std::cout << "Added initial node" << std::endl;
+			update();
 		}
 		else
 		{
@@ -81,39 +88,146 @@ public:
 				SkipListNode* nextNode = 
 					curNode->nodes[curHeight];
 				while (nextNode && nextNode->value < val)
+				{
 					curNode = curNode->nodes[curHeight];
+					nextNode = curNode->nodes[curHeight];
+				}
 				--curHeight;
 			}
 
 			//reached base level
-			SkipListNode* nextNode = 
-				curNode->nodes[0];
+			SkipListNode* nextNode = curNode->nodes[0];
 			while (nextNode && nextNode->value < val)
+			{
 				curNode = nextNode;
+				nextNode = curNode->nodes[0];
+			}
 
-			
+			insert(val, curNode);
+			update();
 		}
 	}
 
 	// Remove ALL occurences of 'val'
-	void erase(T const& val);
+	void erase(T const& val)
+	{
+		size_t curHeight = MaxHeight-1;
+		while (!m_entryPoints[curHeight] && curHeight != 0)
+			--curHeight;
+
+		if (!m_entryPoints[curHeight])
+		{
+			// we're empty so trivially returning
+			return;
+		}
+		else
+		{
+			SkipListNode* curNode = m_entryPoints[curHeight];
+
+			while (curHeight != 0)
+			{
+				assert(curNode->numNodes >= curHeight);
+
+				SkipListNode* nextNode = 
+					curNode->nodes[curHeight];
+				while (nextNode && nextNode->value < val)
+				{
+					curNode = curNode->nodes[curHeight];
+					nextNode = curNode->nodes[curHeight];
+				}
+				--curHeight;
+			}
+
+			//reached base level
+			SkipListNode* nextNode = curNode->nodes[0];
+			while (nextNode && nextNode->value < val)
+			{
+				curNode = nextNode;
+				nextNode = curNode->nodes[0];
+				update();
+			}
+
+			if (nextNode && nextNode->value == val)
+			{
+				// we have matches
+				while (nextNode && nextNode->value == val)
+					nextNode = nextNode->nodes[0];
+
+				SkipListNode* tmp = curNode->nodes[0];
+				while (tmp != nextNode)
+				{
+					SkipListNode* nextTmp = tmp->nodes[0];
+					delete tmp;
+					tmp = nextTmp;
+				}
+				curNode->nodes[0] = nextNode;
+				update();
+			}
+			else
+			{
+				// no matches, trivially returning;
+				return;
+			}
+		}
+	}
 
 private:
 	size_t const m_maxHeight = MaxHeight;
 	SkipListNode* m_entryPoints[MaxHeight];
 
 	// Insert value after node
-	void insert(T const& val, SkipListNode const& node)
+	void insert(T const& val, SkipListNode* node)
 	{
+		SkipListNode* newNode = createNode(val);
+		newNode->nodes[0] = node->nodes[0];
+		node->nodes[0] = newNode;
 	}
 
 	// Creates a new node of random height
 	SkipListNode* createNode(T const& val)
 	{
-		SkipListNode* newNode = new SkipListNode(rand() % MaxHeight, val);
+		size_t numNodes = std::max(1UL, rand() % MaxHeight);
+		SkipListNode* newNode = new SkipListNode(numNodes, val);
 		std::memset(newNode->nodes, 0, sizeof(SkipListNode*)*newNode->numNodes);
 
 		return newNode;
+	}
+
+	// update SkipList structure
+	void update(void)
+	{
+		// Update entry points first
+		SkipListNode* curNode = m_entryPoints[0];
+		std::memset(m_entryPoints, 0, sizeof(SkipListNode*)*MaxHeight);
+
+		size_t targetHeight = 0;
+		while (curNode && targetHeight < MaxHeight)
+		{
+			if (curNode->numNodes >= targetHeight)
+			{
+				for (size_t i=targetHeight; i<curNode->numNodes; ++i)
+					m_entryPoints[i] = curNode;
+				targetHeight = curNode->numNodes+1;
+			}
+
+			curNode = curNode->nodes[0];
+		}
+
+		SkipListNode* ptrs[MaxHeight];
+		std::copy(m_entryPoints, m_entryPoints+MaxHeight, ptrs);
+		curNode = m_entryPoints[0];
+		while (curNode)
+		{
+			size_t numNodes = curNode->numNodes;
+			for (size_t i=0; i<numNodes; ++i)
+				if (ptrs[i] && ptrs[i] != curNode)
+					ptrs[i]->nodes[i] = curNode;
+
+			for (size_t i=0; i<numNodes; ++i)
+				ptrs[i] = curNode;
+
+			curNode = curNode->nodes[0];
+		}
 	}
 };
 
@@ -129,11 +243,23 @@ std::ostream& operator<<(std::ostream& os, SkipList<T,MaxHeight> const& skipList
 
 int main(int argc, char* argv[])
 {
+	//srand(time(0));
+
 	SkipList<int> testList;
 
 	std::cout << "Initial SkipList: " << testList << std::endl;
-	for (int i=0; i<5; ++i)
-		testList.insert(i);
+	for (int i=0; i<1000; ++i)
+	{
+		int rnd = rand() % 1000;
+		testList.insert(rnd);
+	}
+
+	for (int i=0; i<1000; ++i)
+	{
+		int rnd = rand() % 1000;
+		testList.erase(rnd);
+	}
+
 	std::cout << "Final SkipList: " << testList << std::endl;
 
 	return 0;
